e2389bf6d9f306e0b4e7d126bee1c161377072b2
Rayan Kemayou <fredy.rayan@gmail.com>
2018-02-14 11:22:11 +0100

    Version 2.0

--- fragment/navbarre.php
+++ fragment/navbarre.php
@@ -1,69 +1,30 @@
-<!-- Navigation -->
-    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
-        <div class="container-fluid" style="background: rgba(0,0,0,0.4);color: #fff;width: 100%;">
-  
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll" >
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">IFCAD</span>
-                    Menu <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="./index.php">Ifcad asbl</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" >
-                <ul class="nav navbar-nav navbar-right">
-                    <li style="border-bottom: 7px solid red">
-                        <a href="./index.php">Accueil</a>
-                    </li>
-                    <li style="border-bottom:7px solid purple">
-                        <a href="ca.php">Conseil d'administration</a>
-                    </li>
-                    <li style="border-bottom:7px solid #B7A53E">
-                        <a href="presentation.php">Pr&eacute;sentation</a>
-                    </li>
-                    <li style="border-bottom:7px solid #69A940">
-                        <a href="inscription.php#formulaire">Inscription</a>
-                    </li>
-                    <li class="dropdown" style="border-bottom: 7px solid #4D8CAB">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Connexion <span class="glyphicon glyphicon-chevron-down"></span></i></a>
-                        <ul class="dropdown-menu" style="border-radius:6px;background-color: rgba(250,250,250,0.5);margin-top:1px">
-                    
-                        <li>
-                        <a href="" data-toggle="modal" data-target="#myModal1">Enseignant(e)s</a>
-                    </li>
-                    <li>
-                        <a href="http://www.platforme.ifcad.net">&Eacute;tudiant(e)s</a>
-                    </li>
-                </ul>
-                    </li>
-                    <li style="border-bottom: 7px solid black">
-                        <a href="cours.php">Cours</a>
-                    </li>
-                    <li style="border-bottom: 7px solid #306956">
-                        <a href="actu.php">Actualit&Eacute;s</a>
-                    </li>
-                    <li class="dropdown" style="border-bottom: 7px solid #B4357B">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">AIDE <span class="glyphicon glyphicon-chevron-down"></span></i></a>
-                        <ul class="dropdown-menu" style="border-radius:6px;background-color: rgba(250,250,250,0.5);margin-top:1px">
-                    
-                        <li>
-                        <a href="info.php">Le s&Eacute;jour en belgique</a>
-                    </li>
-                </ul>
-                    </li>
-                    
-                    
-                    <li style="border-bottom: 7px solid #E18024">
-                        <a href="contact.php">Contact</a>
-                    </li>
-                    
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
+<nav class="navbar navbar-down">
+    <div class="container">
+      <div class="flex-container">  
+        <div class="navbar-header flex-item">
+          <div class="navbar-brand" href="https://github.com/alphadsy">Y-CASH</div>
         </div>
-        <!-- /.container -->
+        <ul class="nav navbar-nav flex-item hidden-xs">
+          <li><a href="https://github.com/alphadsy">Home</a></li>
+          <li><a href="https://github.com/alphadsy">Get Started</a></li> 
+          <li><a href="https://github.com/shu3aybbadran">Documentation</a></li> 
+        </ul>
+        <ul class="nav navbar-nav flex-item hidden-xs pull-right">
+          <li><a href="#" class="">contact us</a></li> 
+        </ul>
+        <!-- dropdown only moblie -->
+          <div class="dropdown visible-xs pull-right">
+            <button class="btn btn-default dropdown-toggle " type="button" id="dropdownmenu" data-toggle="dropdown">
+              <span class="glyphicon glyphicon-align-justify"></span> 
+            </button>
+            <ul class="dropdown-menu">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Get Started</a></li> 
+              <li><a href="#">Documentation</a></li> 
+              <li role="separator" class="divider"></li>
+              <li><a href="#">contact us</a></li>
+            </ul>
+          </div>
+        </div>  
+      </div>
     </nav>
-
-    <!-- Page Header -->
--- index.php
+++ index.php
@@ -4,24 +4,331 @@ include("manifest/pash.php");
 <!DOCTYPE html>
 <html ng-app="Y-CASH">
 <head>
-	<meta charset="utf-8">
-  	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="<?php echo $pash['bootstrap_css']?>">
+
 	<title>Y-CASH</title>
-	<script type="text/javascript" src="<?php echo $pash['jquery_js']?>"></script>
-	<script type="text/javascript" src="<?php echo $pash['bootstrap_js']?>"></script>
-	<script type="text/javascript" src="<?php echo $pash['angular']?>"></script>
-	<script type="text/javascript" src="<?php echo $pash['app']?>"></script>
-	<script type="text/javascript" src="<?php echo $pash['contextMenu']?>"></script>
+
+  <link href="./res/css/css.css" rel="stylesheet" type="text/css">
+  <link href="./res/css/toolkit-startup.css" rel="stylesheet">
+  <link href="./res/css/application-startup.css" rel="stylesheet">
+  <link href="./res/css/style_home.css" rel="stylesheet">
+
 
 </head>
-<body ng-controller="HomeCtrl" style="height: 20000px;">
+<body ng-controller="HomeCtrl">
 
-	<!-- nav barre  -->
-	<?php include("fragment/navbarre.php") ?>
-	<!-- end nav barre  -->
+<div class="bsa bsb hidden" id="sidebar">
+  <ul class="nav bpi nav-stacked yl">
+    <li class="azb">Y-CASH</li>
+    <li class="m">
+      <a class="qm active" href="https://bootstrap-themes.github.io/marketing/index.html">Home</a>
+    </li>
+    <li class="m">
+      <a class="qm" href="https://bootstrap-themes.github.io/marketing/minimal/index.html">Get Started</a>
+    </li>
+    <li class="m">
+      <a class="qm" href="https://bootstrap-themes.github.io/marketing/bold/index.html">Doc</a>
+    </li>
+    <li class="nav-divider"></li>
+    <li class="azb">Docs</li>
+    <li class="m">
+      <a class="qm" href="https://bootstrap-themes.github.io/marketing/docs/index.html">Contast us</a>
+    </li>
+  </ul>
+</div>
+<div class="brz" id="stage">
+	<div class="k am bqa bse" style="background-image: url(res/img/back1.png);">
+		  <div class="e aij aey app-navbar">
+  <nav class="du bqq azg qz">
+    <a class="l aiy" href="https://bootstrap-themes.github.io/marketing/">
+      <strong style="background: #fff; padding: 12px; border-radius: 4px; color: #28669F;">go</strong>
+    </a>
+    <button class="qv" type="button" data-target="#stage" data-toggle="stage" data-distance="-250">
+      <span class="qw"></span>
+    </button>
 
-<center><h1>HOME</h1></center>
+    <div class="d-none wx axv">
+      <ul class="navbar-nav">
+        <li class="m ahq ">
+          <a class="qm" href="https://bootstrap-themes.github.io/marketing/#">Home</a>
+        </li>
+        <li class="m ahq ">
+          <a class="qm" href="https://bootstrap-themes.github.io/marketing/minimal/index.html">Get Started</a>
+        </li>
+        <li class="m ahq ">
+          <a class="qm" href="https://bootstrap-themes.github.io/marketing/bold/index.html">Doc</a>
+        </li>
+        <li class="m ahq ">
+          <a class="qm" href="https://bootstrap-themes.github.io/marketing/docs/index.html">Contast us</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</div>
+
+	  <img class="z" src="./res/img/startup-0.svg">
+
+  <div class="bqg ait">
+    <div class="e">
+      <div class="c">
+        <div class="fv gt">
+          <img src="./res/img/logohd_noback.png" class="logohd_noback">
+          <h1 class="bsf bsg">Everyone can pay cash.</h1>
+          <p class="al agw an">Integrate the fastest and safest mobile payment method to optimize sales across all your platforms.</p>
+          <button class="dm ox ap">Try it now</button>
+        </div>
+      </div>
+    </div>
+  </div>
+		
+	</div>
+	<div class="k block-secondary aa">
+  <div class="e">
+    <div class="c aq">
+
+      <div class="fq d-none wx">
+        <img class="bsh" src="./res/img/startup-2.png" style="width: 100%;">
+      </div>
+
+      <div class="gf fx ajb">
+        <h6 class="an axv">Rich Information</h6>
+        <h2>Make informed decisions with historical &amp; real time data.</h2>
+        <p class="al agw">We combine real-time events with rich historical data to help you answer the toughest questions about growth and profitability of your products.</p>
+        <div class="c d-none xb">
+          <div class="fr agp">
+            <h5>Data frequency</h5>
+            <p>We poll for data on a millisecond basis. You can react to new information in seconds rather than days. <a href="https://bootstrap-themes.github.io/marketing/#" class="ayc">Learn more.</a></p>
+          </div>
+          <div class="fr">
+            <h5>Reliability &amp; uptime</h5>
+            <p>We process our data across a massively distributed network of reliable servers to ensure 99.99% uptime, always. <a href="https://bootstrap-themes.github.io/marketing/#" class="ayc">Learn more</a>.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="k am block-secondary ab">
+  <div class="e">
+    <div class="c aq">
+      <div class="gf hu jv">
+        <h6 class="an axv">Easy development</h6>
+        <h2>Integreation is very simple.</h2>
+        <p class="al agw an">the integration of our module is very simple and to bring it to all the developers.</p>
+        <button class="dm oz ap ayu">
+          Read our docs
+        </button>
+      </div>
+      <div class="ge ajb ht jw">
+        <pre class="ac">
+<span>1</span>&lt;html&gt;
+<span>2</span>&lt;head&gt;
+<span>3</span>.
+<span>4</span>&lt;title&gt;My web app&lt;/title&gt;
+<span>5</span>.  
+<span>6</span>&lt;/head&gt;     
+<span>7</span>&lt;body&gt;    
+<span>8</span>.    
+<span>9</span>. 
+<span>10</span>.     
+<span>11</span>&lt;form method="get" data-ycash="form" button class="" 
+<span> </span>  action="https://api.y-cash.com/pay/v1.0/
+<span> </span>  DkNmCawD8YVYbkTglRJNrKCdvRhX3S"&gt;
+<span>12</span> type="submit"&gt;Y-cash pay&lt;/button&gt;&lt;/form&gt;    
+<span>13</span>.      
+<span>14</span>.      
+<span>15</span>&lt;script type="text/javascript" src="https://fr.y-cash.com/widget/v1/y-cash.min.js"&gt;&lt;/script&gt;
+<span>16</span>&lt;/body&gt;
+<span>17</span>&lt;/html&gt;</pre>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="k am ad" style="background-image: url(res/img/startup-3.jpg);" style="min-width: 1500px;">
+    <div class="e">
+      <div class="c aq aic yt">
+        <div class="fs ge aiq" style="float: left;">
+          <h6 class="an axv agi">High Praise</h6>
+          <h2 class="agw">“Since the integration of Y-CASH module, my sales have literally exploded and my business has opened to all the population.”</h2>
+          <p class="agw an">Cecilia wangue, founder of Ket-up Startup</p>
+        </div>
+      </div>
+  </div>
+</div>
+
+<div class="k ae aiq">
+  <div class="e ays">
+    <img src="./res/img/sanzo.png" style="max-height: 50px">
+    <img src="./res/img/gms.png" style="max-height: 50px">
+    <img src="./res/img/like-consept.png" style="max-height: 50px">
+    <img src="./res/img/devmy.png" style="max-height: 50px">
+    <img src="./res/img/logo-ketup.png" style="max-height: 50px">
+  </div>
+</div>
+
+<div class="k block-secondary af">
+  <div class="e ays">
+
+    <div class="c ahd yu">
+      <div class="fi ft gt">
+        <h6 class="an axv agi">Inside the machine</h6>
+        <h2 class="agw">It’s not hard to see how we make your life easier every day.</h2>
+      </div>
+    </div>
+
+    <div class="c ag">
+      <div class="gd ail ahd">
+        <img class="agb" src="./res/img/startup-9.svg">
+        <p><strong>24/7 support.</strong> We’re always here for you no matter what time of day.</p>
+      </div>
+      <div class="gd ail ahd">
+        <img class="agb" src="./res/img/startup-10.svg">
+        <p><strong>E-commerce.</strong> We automatically handle all sales analytics.</p>
+      </div>
+      <div class="gd ail ahd">
+        <img class="agb" src="./res/img/startup-11.svg">
+        <p><strong>Turnaround.</strong> Our data analysis is distributed, so it processes in seconds.</p>
+      </div>
+    </div>
+
+    <div class="c ag">
+      <div class="gd ail ahd">
+        <img class="agb" src="./res/img/startup-12.svg">
+        <p><strong>Rich calculations.</strong> Limitless ways to splice and dice your data.</p>
+      </div>
+      <div class="gd ail ahd">
+        <img class="agb" src="./res/img/startup-13.svg">
+        <p><strong>Mobile apps.</strong> iOS and Android apps available for monitoring.</p>
+      </div>
+      <div class="gd ail ahd">
+        <img class="agb" src="./res/img/startup-14.svg">
+        <p><strong>Secure connections.</strong> Every single request is routed through HTTPS.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="k aj">
+  <div class="e axh">
+
+    <div class="c ahd yu">
+      <div class="fi gh gt">
+        <h6 class="an axv agi">Business Talk</h6>
+        <h2>No plans. We just bump your plan whenever you need it.</h2>
+      </div>
+    </div>
+
+    <div class="c">
+      <div class="gd ail ahy ahd">
+        <div class="ahx agi">
+          <h6 class="an axv agw">Personal</h6>
+          <img class="agw" src="./res/img/startup-15.svg">
+          <p class="ahy">Plenty of processing power for any personal projects, big or small.</p>
+        </div>
+
+        <ul class="ek ayo ayq agt">
+          <li class="aij"><strong>10k</strong> monthly requests</li>
+          <li class="aij"><strong>9am-5pm</strong> technical supprt</li>
+          <li class="aij"><strong>Public</strong> API access</li>
+        </ul>
+
+        <button class="dm ap ox btn-block">
+          Start <span class="d-none xn">a personal account</span>
+        </button>
+      </div>
+
+      <div class="gd ail ahy ahd">
+        <div class="ahx">
+          <h6 class="an axv agw">Business</h6>
+          <img class="agw" src="./res/img/startup-16.svg">
+          <p class="ahy">The perfect sized plan for small businesses to get started.</p>
+        </div>
+
+        <ul class="ek ayo ayq agt">
+          <li class="aij"><strong>100k</strong> monthly requests</li>
+          <li class="aij"><strong>24/7</strong> technical supprt</li>
+          <li class="aij"><strong>Public</strong> API access</li>
+        </ul>
+
+        <button class="dm ap ox btn-block">
+          Start <span class="d-none xn">a business account</span>
+        </button>
+      </div>
+
+      <div class="gd ail ahr ahd">
+        <div class="ahx">
+          <h6 class="an axv agw">Corporate</h6>
+          <img class="agw" src="./res/img/startup-17.svg">
+          <p class="ahy">An unlimited plan that will scale infinitely to any size project.</p>
+        </div>
+
+        <ul class="ek ayo ayq agt">
+          <li class="aij"><strong>Unlimited</strong> monthly requests</li>
+          <li class="aij"><strong>24/7</strong> technical supprt</li>
+          <li class="aij"><strong>Public &amp; Private</strong> API access</li>
+        </ul>
+
+        <button class="dm ap ox btn-block">
+          Start <span class="d-none xn">a corporate account</span>
+        </button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+<div class="k am ak">
+ <div class="e">
+    <div class="c">
+      <div class="ge ahd">
+        <ul class="ek ayn">
+          <li class="agi"><h6 class="axv">About</h6></li>
+          <li class="an">
+             We’ve been working on Go Analytics for the better part of a decade and are super proud of what we’ve created. If you’d like to learn more, or are interested in a job, contact us anytime at <a href="mailto:%20themes@getbootstrap.com">themes@getbootstrap.com</a>.
+          </li>
+        </ul>
+      </div>
+      <div class="gb ajb ahd">
+        <ul class="ek ayn">
+          <li class="agi"><h6 class="axv">Product</h6></li>
+          <li class="an">Features</li>
+          <li class="an">Examples</li>
+          <li class="an">Tour</li>
+          <li class="an">Gallery</li>
+        </ul>
+      </div>
+      <div class="gb ahd">
+        <ul class="ek ayn">
+          <li class="agi"><h6 class="axv">Apis</h6></li>
+          <li class="an">Rich data</li>
+          <li class="an">Simple data</li>
+          <li class="an">Real time</li>
+          <li class="an">Social</li>
+        </ul>
+      </div>
+      <div class="gb ahd">
+        <ul class="ek ayn">
+          <li class="agi"><h6 class="axv">Legal</h6></li>
+          <li class="an">Terms</li>
+          <li class="an">Legal</li>
+          <li class="an">Privacy</li>
+          <li class="an">License</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
+
+<script src="./res/js/jquery.min.js"></script>
+    <script src="./res/js/popper.min.js"></script>
+    <script src="./res/js/toolkit.js"></script>
+    <script src="./res/js/application.js"></script>
 
 </body>
 </html>
